35f2ef9c9ea12832c956548b4d05633c0a53506a
mohitbhati03 <mohitbhati130306@gmail.com>
2025-06-29 12:13:54 +0530

    frontend

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Tomato is an online food ordering system that helps customers explore menus, place orders, and enjoy delicious meals — all with just a few clicks. Simple. Smart. Satisfying.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-333-420-0333</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2025 © Tomato.com - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
